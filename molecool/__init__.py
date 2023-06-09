1f75a13a4df00f2bb57ac3014fcae3930f870660
Nishant Giridhar <nishant.vnk@gmail.com>
2023-06-09 13:13:03 -0400

    Updates to documentation

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -1,4 +1,6 @@
-"""A Python package for analyzing and visualizing xyz files."""
+"""A Python package for analyzing and visualizing xyz files.
+This was created for the Python Best Practices Workshop
+"""
 
 # Add imports here
 from .functions import canvas
